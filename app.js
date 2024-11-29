8fba526e170dc7b64a1d3d5855f0b59f1ba9af30
Abuaisha-7 <heyrubedru8086@gmail.com>
2024-11-29 06:43:42 -0800

    second commit

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.render("home", {
-        homeStartingContent: "Home",
-        
+        startingContent: homeStartingContent
     }); 
 
 });
