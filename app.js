4c7eec2f646d92b5386a6f4e3c570def9fa7727b
Abuaisha-7 <heyrubedru8086@gmail.com>
2024-12-29 06:47:44 -0800

    adding mongodb database

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require('lodash');
-
-let posts = [];
 
 const homeStartingContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.";
 const aboutContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.";
@@ -16,12 +15,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb+srv://admin-heyru:Test123@cluster0.dqdps.mongodb.net/blogDB");
+
+const postSchema = {
+    title: String,
+    content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
 app.get("/", function(req, res){
-    res.render("home", {
-        startingContent: homeStartingContent,
-        posts: posts
-    }); 
+
+    Post.find().then(function(foundPosts){
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: foundPosts
+        });
+    });
     
+
 });
 
 app.get("/about", function(req, res){
@@ -43,26 +57,38 @@ app.get("/compose", function(req, res){
 
 app.get("/posts/:postName", function(req, res){
     const requestedTitle = _.lowerCase (req.params.postName);
+
+    Post.find().then(function(foundPosts){
+
+        foundPosts.forEach(function(post){
+            const storedTitle = _.lowerCase (post._id);
+            if(storedTitle === requestedTitle){
+                res.render("post", {
+                 post: post
+                });
+            } 
+        })
+
+    });
         
-    posts.forEach(function(post){
-        const storedTitle = _.lowerCase (post.title);
-        if(storedTitle === requestedTitle){
-            res.render("post", {
-             post: post
-            });
-        } 
-    })
   
 });
 
 app.post("/compose", function(req,res){
    
-    const post = {
+    const newPost = new Post({
         title : req.body.postTitle,
         content : req.body.postBody
+    });
+
+    if(newPost.title === "" || newPost.content === ""){
+        res.redirect("/compose");
+    } else {
+        newPost.save();
+        res.redirect("/");
     }
-    posts.push(post);
-    res.redirect("/");
+   
+    
 })
 
 
